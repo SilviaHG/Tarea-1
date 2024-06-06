c5ad443f6a03be5d7cfad3535138544341158104
SilviaHG <sahg1997@gmail.com>
2024-06-06 15:46:05 -0600

    Tarea #1
    Tarea de practica de aplicaciones de escritorio

---
+++ Form1.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace Tarea1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Maroon;
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Location = new System.Drawing.Point(74, 76);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 49);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "#1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Maroon;
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button2.Location = new System.Drawing.Point(178, 76);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(86, 49);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "#2";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Maroon;
+            this.button3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button3.Location = new System.Drawing.Point(288, 76);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(86, 49);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "#3";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Maroon;
+            this.button4.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button4.Location = new System.Drawing.Point(389, 76);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(86, 49);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "#4";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Maroon;
+            this.button5.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button5.Location = new System.Drawing.Point(490, 76);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(86, 49);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "#5";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Maroon;
+            this.button6.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button6.Location = new System.Drawing.Point(74, 131);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(86, 49);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "#6";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.Maroon;
+            this.button7.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button7.Location = new System.Drawing.Point(178, 131);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(86, 49);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "#7";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.Maroon;
+            this.button8.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button8.Location = new System.Drawing.Point(288, 131);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(86, 49);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "#8";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.Maroon;
+            this.button9.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button9.Location = new System.Drawing.Point(389, 131);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(86, 49);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "#9";
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.Color.Maroon;
+            this.button10.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button10.Location = new System.Drawing.Point(490, 131);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(86, 49);
+            this.button10.TabIndex = 9;
+            this.button10.Text = "#10";
+            this.button10.UseVisualStyleBackColor = false;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.BackColor = System.Drawing.Color.Maroon;
+            this.button11.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button11.Location = new System.Drawing.Point(74, 186);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(86, 49);
+            this.button11.TabIndex = 10;
+            this.button11.Text = "#11";
+            this.button11.UseVisualStyleBackColor = false;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.BackColor = System.Drawing.Color.Maroon;
+            this.button12.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button12.Location = new System.Drawing.Point(178, 186);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(86, 49);
+            this.button12.TabIndex = 11;
+            this.button12.Text = "#12";
+            this.button12.UseVisualStyleBackColor = false;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button13
+            // 
+            this.button13.BackColor = System.Drawing.Color.Maroon;
+            this.button13.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button13.Location = new System.Drawing.Point(288, 186);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(86, 49);
+            this.button13.TabIndex = 12;
+            this.button13.Text = "#13";
+            this.button13.UseVisualStyleBackColor = false;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // button14
+            // 
+            this.button14.BackColor = System.Drawing.Color.Maroon;
+            this.button14.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button14.Location = new System.Drawing.Point(389, 186);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(86, 49);
+            this.button14.TabIndex = 13;
+            this.button14.Text = "#14";
+            this.button14.UseVisualStyleBackColor = false;
+            this.button14.Click += new System.EventHandler(this.button14_Click);
+            // 
+            // button15
+            // 
+            this.button15.BackColor = System.Drawing.Color.Maroon;
+            this.button15.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button15.Location = new System.Drawing.Point(490, 186);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(86, 49);
+            this.button15.TabIndex = 14;
+            this.button15.Text = "#15";
+            this.button15.UseVisualStyleBackColor = false;
+            this.button15.Click += new System.EventHandler(this.button15_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Maroon;
+            this.label1.Location = new System.Drawing.Point(240, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(178, 45);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Tarea #1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(671, 261);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,312 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tarea1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            /*
+             * 1.	Declara e inicializa un array de enteros 
+             *      llamado numeros con los valores {10, 20, 30, 40, 50}. Luego,
+             *       imprime su longitud.
+             * */
+            int[] numeros = { 10, 20, 30, 40, 50 };
+            MessageBox.Show($"La Longitud es de: {numeros.Length} ");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            /*
+             * 2.	Dado el array numeros = {5, 10, 15, 20, 25}, 
+             *      escribe un código que establezca el valor del tercer elemento (índice 2) a 100.
+             *      Luego, imprime el array completo.
+             * */
+            int[] numeros = { 5, 10, 15, 20, 25 };
+
+            
+
+            DialogResult optUser = MessageBox.Show($"Desea modificar el indice 2 '{numeros[2]}' por un 100?",
+            "Advertencia", MessageBoxButtons.YesNo);
+
+            switch (optUser)
+            {
+                case DialogResult.Yes:
+                    numeros[2] = 100;
+
+                    string msg = string.Join(Environment.NewLine, numeros);
+                    MessageBox.Show($"{msg}");
+
+                break;
+
+                case DialogResult.No:
+
+                    string ms = string.Join(Environment.NewLine, numeros);
+                    MessageBox.Show($"{ms}");
+                    
+                break;
+            }
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            /*
+             * 3.	Crea un array de strings llamado nombres con los valores
+             * {"Ana", "Luis", "Pedro", "Marta"}.
+             * Busca el índice del nombre "Pedro" e imprímelo.
+             * */
+
+            string[] nombres = { "Ana", "Luis", "Pedro", "Marta" };
+
+            foreach (string buscar in nombres)
+            {
+                if (buscar == "Pedro")
+                {
+                    MessageBox.Show($"Encontramos el nombre: {buscar}");
+                    
+                }
+            }
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            /*
+             * 4.	Dado el array numeros = {50, 40, 30, 20, 10}, 
+             * escribe un código que ordene el array en orden ascendente y luego lo imprima.
+             * */
+
+            int[] numeros = { 50, 40, 30, 20, 10 };
+            Array.Sort(numeros); // ordeno de manera ascendente
+            string msg = string.Join(Environment.NewLine, numeros); // para convertirlos en una sola cadena
+            MessageBox.Show($"{msg}");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            /*
+             * 5.	Escribe un código que invierta el array 
+             * numeros = {1, 2, 3, 4, 5} y luego imprima los elementos en el nuevo orden.
+             * */
+            int[] numeros = { 1, 2, 3, 4, 5 };
+            Array.Reverse(numeros); // invierto el array
+            string msg = string.Join(Environment.NewLine, numeros); // para convertirlos en una sola cadena
+            MessageBox.Show($"{msg}");
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            /*
+             * 6.	Dado el array numeros = {10, 20, 30, 40, 50}, 
+             * usa el método Array.Clear para establecer a cero los elementos 
+             * desde el índice 1 hasta el índice 3. Luego, imprime el array.
+             * */
+
+            int[] numeros = { 10, 20, 30, 40, 50 };
+            
+            for (int i = 1; i < numeros[3]; i++)
+            {
+                
+                Array.Clear(numeros, 1, 3);
+            }
+
+            string msg = string.Join(Environment.NewLine, numeros); // para convertirlos en una sola cadena
+            MessageBox.Show($"{msg}");
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            /*
+             * 7.	Crea dos arrays de enteros origen y destino.
+             * Copia todos los elementos de origen = {1, 2, 3, 4, 5} a destino y 
+             * luego imprime destino.
+             * */
+
+            int [] origen = { 1,2, 3, 4,5 }; // creamos el array lleno
+            int[] destino = new int[origen.Length]; // creamos un array con el mismo tamaño del origen
+
+            Array.Copy(origen, destino, origen.Length); // copeamos el array de origen a destino
+
+
+            string msg = string.Join(Environment.NewLine, destino); // para convertirlos en una sola cadena
+            MessageBox.Show($"{msg}");
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            /*
+             * 8.	Escribe un código que encuentre el primer número mayor 
+             * que 20 en el array numeros = {10, 20, 30, 40, 50} utilizando el método
+             * Array.Find. Imprime el número encontrado.
+             * */
+
+            int [] numeros = {10,20, 30, 40,50};
+
+            int mayor20 = Array.Find(numeros, x => x > 20);
+
+            MessageBox.Show($"{mayor20}");
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            /*
+             * 9.	Crea un array de enteros llamado valores con los valores 
+             *      {1, 3, 5, 7, 9}. Escribe un código que verifique si existe un número
+             *       mayor que 6 en el array utilizando Array.Exists e imprime el resultado
+             *      (true/false).
+             * */
+
+            int[] valores = { 1, 3, 5, 7, 9 };
+
+            bool existe = Array.Exists(valores, x => x > 6);
+
+            if ( existe )  {
+                MessageBox.Show($"{existe}");
+            }
+            else
+            {
+                MessageBox.Show($"{existe}");
+            }
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            /*
+             * 10.	Dado el array numeros = {2, 4, 6, 8, 10}, usa el método
+             * Array.Find para buscar el primer número par en el array.
+             * Imprime el número encontrado.
+             * */
+
+            int[] numeros = { 1, 3, 7, 8, 10 };
+
+            int find = Array.Find(numeros, s => s %2 == 0);
+
+            MessageBox.Show($"{find}");
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            /*
+             * 11.	Crea un array de strings llamado frutas con los valores 
+             * {"manzana", "banana", "naranja", "kiwi"}. Usa el método Array.IndexOf 
+             * para encontrar el índice de "kiwi" e imprímelo.
+             * */
+
+            string[] valores = { "manzana", "banana", "naranja", "kiwi" };
+
+            int find = Array.IndexOf(valores, "kiwi");
+
+            MessageBox.Show($"{find}");
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            /*
+             * 12.	Dado el array numeros = {15, 25, 35, 45, 55}, usa el método
+             * Array.Clear para establecer a cero los elementos desde el índice 
+             * 2 hasta el índice 4. Luego, imprime el array completo.
+             * */
+
+
+            int[] numeros = { 15, 25, 35, 45, 55 };
+
+            for (int i = 0; i < numeros.Length; i++)
+            {
+
+                Array.Clear(numeros, 2, 4-1);
+            }
+
+            string msg = string.Join(Environment.NewLine, numeros); // para convertirlos en una sola cadena
+            MessageBox.Show($"{msg}");
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            /*
+             * 13.	Crea un array de enteros llamado datos con los valores 
+             * {5, 10, 15, 20, 25}. Copia los primeros 3 elementos de datos a 
+             * un nuevo array llamado subDatos y luego imprime subDatos.
+             * */
+
+            int[] datos = { 5, 10, 15, 20, 25 };
+            int[] subDatos = new int[3];
+
+            Array.Copy(datos, subDatos, 3);
+
+            string msg = string.Join(Environment.NewLine, subDatos); // para convertirlos en una sola cadena
+            MessageBox.Show($"{msg}");
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            /*
+             * 14.	Utiliza el método Array.Reverse para invertir 
+             * el orden de los elementos en el array valores = {3, 6, 9, 12, 15} 
+             * y luego imprime el resultado.
+             * */
+
+            int[] datos = { 3, 6, 9, 12, 15 };
+
+            Array.Reverse(datos);
+
+            string msg = string.Join(Environment.NewLine, datos); // para convertirlos en una sola cadena
+            MessageBox.Show($"{msg}");
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            /*
+             * 15.	Escribe un código que verifique si el número 10 está presente 
+             * en el array numeros = {5, 10, 15, 20, 25} usando el método Array.Exists.
+             * Imprime el resultado (true/false).
+             * */
+
+            int[] datos = { 5, 10, 15, 20, 25 };
+
+            bool existeTEN = Array.Exists(datos, s => s == 10);
+            if (existeTEN)
+            {
+                MessageBox.Show($"{existeTEN}");
+            }
+            else
+            {
+                MessageBox.Show($"{existeTEN}");
+            }
+
+            // Obtener el valor mínimo 
+            int mini = datos.Min();
+            MessageBox.Show($"Valor mínimo: {mini}");
+
+            // Obtener el valor máximo 
+            int maximo = datos.Max(); 
+            MessageBox.Show($"Valor máximo: {maximo}");
+
+            // Obtener la suma 
+            int sumaTotal = datos.Sum();
+            MessageBox.Show($"La suma total es: {sumaTotal}");
+        }
+    }
+}
